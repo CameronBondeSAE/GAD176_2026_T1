a47326b27359e98369c44a13ddbd2840e8ef737c
CameronBondeSAE <c.bonde@sae.edu>
2026-02-24 14:35:06 +1100

    Ray tests

--- Assets/Scripts/Button.cs
+++ Assets/Scripts/Button.cs
@@ -2,16 +2,22 @@ using CameronBonde;
 using UnityEngine;
 
 
-// NOTE: The 'partial' keyword here is just for the Interfaces plugin to work
-public partial class Button : MonoBehaviour, IInteractable
+namespace CameronBonde
 {
-	// Add this to any interface variables to enable drag-drop in Unity
-	[SerializeInterface]
-	public IInteractable thingToInteractWith;
+	// NOTE: The 'partial' keyword here is just for the Interfaces plugin to work
+	public class Button : MonoBehaviour, IInteractable
+	{
+		// Add this to any interface variables to enable drag-drop in Unity
+		[SerializeInterface]
+		public IInteractable thingToInteractWith;
 
-    public void Interact()
-    {
-	    // eg a Designer could hook this up to a door/light/OTHER switches etc
-	    thingToInteractWith.Interact();
-    }
+		[SerializeInterface]
+		public IInteractable[] thingsToInteractWith;
+
+		public void Interact()
+		{
+			// eg a Designer could hook this up to a door/light/OTHER switches etc
+			thingToInteractWith.Interact();
+		}
+	}
 }
