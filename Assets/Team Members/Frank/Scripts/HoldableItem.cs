76e0f5b9e76318aa95ea0c0fb1aed6227e6ac26d
FrankValastro <1035689@student.sae.edu.au>
2026-04-24 23:10:26 +1000

    Set up for implementing holdable objects

---
+++ Assets/Team Members/Frank/Scripts/HoldableItem.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using Frank;
+
+public class HoldableItem : MonoBehaviour, IInteractable, IHoldable
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Interact()
+    {
+        
+    }
+}
