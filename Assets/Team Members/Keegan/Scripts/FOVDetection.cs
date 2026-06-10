0632f3382fc32159b3bfca95dba0cdebc8b8824f
KeeganMiller <126998795+KeeganMiller@users.noreply.github.com>
2026-06-10 11:29:11 +1000

    Emptied class to try different technic

--- Assets/Team Members/Keegan/Scripts/FOVDetection.cs
+++ Assets/Team Members/Keegan/Scripts/FOVDetection.cs
@@ -1,30 +1,28 @@
+using System.Collections.Generic;
+using Anthill.AI;
 using UnityEngine;
 
 public class FOVDetection : MonoBehaviour
 {
-    [Header("Radius Detection")]
-    [SerializeField, Tooltip("The init overlap to detect enemies inside FOV")]
-    private float _detectionRadius;
-    [SerializeField, Tooltip("The transform where to being the radius detection")]
-    private Transform _detectionTransform;
+
 
     #if UNITY_EDITOR
     [SerializeField]
     private bool _drawDebug = true;
     #endif
 
-
+    private void Update()
+    {
+        
+        
+    }
 
 #if UNITY_EDITOR
 
 
     private void OnDrawGizmosSelected()
     {
-        if(_drawDebug && _detectionTransform != null)
-        {
-            Gizmos.color = Color.yellow;
-            Gizmos.DrawWireSphere(_detectionTransform.position, _detectionRadius);
-        }
+
     }
 
 #endif
