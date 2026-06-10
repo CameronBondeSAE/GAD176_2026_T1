f4ddcccb823396ac42b957672af96ecf635bf985
KeeganMiller <126998795+KeeganMiller@users.noreply.github.com>
2026-06-10 11:34:11 +1000

    Added line gizmos with list of directions to move in

--- Assets/Team Members/Keegan/Scripts/FOVDetection.cs
+++ Assets/Team Members/Keegan/Scripts/FOVDetection.cs
@@ -4,25 +4,24 @@ using UnityEngine;
 
 public class FOVDetection : MonoBehaviour
 {
-
-
+    [SerializeField]
+    private List<Vector3> _detectionCastDirections = new List<Vector3>();
     #if UNITY_EDITOR
     [SerializeField]
     private bool _drawDebug = true;
     #endif
-
-    private void Update()
-    {
-        
-        
-    }
 
 #if UNITY_EDITOR
 
 
     private void OnDrawGizmosSelected()
     {
-
+        Gizmos.color = Color.yellow;
+        
+        foreach(Vector3 direction in _detectionCastDirections)
+        {
+            Gizmos.DrawLine(transform.position, transform.position + direction);
+        }
     }
 
 #endif
