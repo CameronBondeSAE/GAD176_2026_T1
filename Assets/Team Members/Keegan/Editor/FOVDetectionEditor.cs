1f8fd50b690f472e6be3be6d3e3ea982b821d33c
KeeganMiller <126998795+KeeganMiller@users.noreply.github.com>
2026-07-15 14:33:47 +1000

    Added editor class

---
+++ Assets/Team Members/Keegan/Editor/FOVDetectionEditor.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+
+namespace Keegan.FOV
+{
+    [CustomEditor(typeof(FOVDetection))]
+    public class FOVDetectionEditor : Editor
+    {
+
+        
+    }
+        
+}
