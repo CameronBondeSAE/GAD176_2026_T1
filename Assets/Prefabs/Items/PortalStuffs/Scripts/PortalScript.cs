fc5b85268c2ead90753e880ca6b4e464924c06d0
MitenPatel-dhunt3er1 <1036081@student.sae.edu.au>
2026-07-30 16:26:21 +1000

    last push, switchinjg to portal branch

---
+++ Assets/Prefabs/Items/PortalStuffs/Scripts/PortalScript.cs
@@ -0,0 +1,14 @@
+using Frank;
+using UnityEngine;
+
+public class PortalScript : MonoBehaviour, IPowerable
+{
+    public GameObject lightning1;
+    public GameObject lightning2;
+    public GameObject lightning3;
+    public GameObject lightning4;
+    public GameObject lightning5;
+    public GameObject lightning6;
+    
+    
+}
