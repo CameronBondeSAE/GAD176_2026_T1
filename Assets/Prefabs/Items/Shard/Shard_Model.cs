53977ab05d0282b72be8e125192e4ee41ebaaff2
CameronBondeSAE <30306370+CameronBondeSAE@users.noreply.github.com>
2026-07-30 18:02:51 +1000

    Refactored IPowered interface for high power devices that require multiple shards etc

--- Assets/Prefabs/Items/Shard/Shard_Model.cs
+++ Assets/Prefabs/Items/Shard/Shard_Model.cs
@@ -3,4 +3,14 @@ using UnityEngine;
 
 public class Shard_Model : MonoBehaviour, IPickup
 {
+	// Variables for totals, to be read by IPowered things
+	
+	public bool UseEnergy(int amount)
+	{
+		// Check for totals
+		// Take off amount from your total
+		// Destroy if zero
+
+		return true; // TODO: This should return false if there's not enough energy to supply the thing
+	}
 }
--- Assets/Team Members/Divij/Scripts/IPowered.cs
+++ Assets/Team Members/Divij/Scripts/IPowered.cs
@@ -3,20 +3,30 @@ using Divij;using UnityEngine;
 
 namespace Divij
 {
-    public interface IPowered
-    {
-        void SetPowered(bool powered);
-        bool GetPowered();
+	public interface IPowered
+	{
+		void SetPowered(bool powered);
+		bool GetPowered();
 
-        /// <summary>
-        ///	Optional
-        /// Energy provider will call this every now and then. Return how much energy you actually used.
-        /// </summary>
-        /// <param name="currentEnergy">total energy you CAN provide</param>
-        /// <returns>used energy</returns>
-        int UseEnergy(int currentEnergy)
-        {
-	        return 0;
-        }
-    }
+		/// <summary>
+		///	Optional
+		/// Energy provider will call this every now and then. Return how much energy you actually used.
+		/// currentEnergy is read directly from the shard reference
+		/// </summary>
+		/// <param name="shard = THIS, so the energy user can ask it how much energy it has"></param>
+		/// <returns>used energy (if no power used yet, return zero)</returns>
+		void ReceivePotentialEnergy(Shard_Model shard)
+		{
+			
+		}
+
+		/// <summary>
+		/// When a shard moves away from an IPowered, it needs to know
+		/// </summary>
+		/// <param name="shard is so you can remove from your list of power providers"></param>
+		void PotentialEnergyRemoved(Shard_Model shard)
+		{
+			
+		}
+	}
 }
