5967d39286cdafdccade7e8599eb1d4909815df9
KeeganMiller <126998795+KeeganMiller@users.noreply.github.com>
2026-07-30 14:18:52 +1000

    Setup editor to have spawn time settings

--- Assets/Prefabs/ShardSpawnSystem/Editor/ShardSpawnControllerEditor.cs
+++ Assets/Prefabs/ShardSpawnSystem/Editor/ShardSpawnControllerEditor.cs
@@ -19,11 +19,20 @@ namespace Keegan.ShardSpawn
         private SerializedProperty groundLayerMaskProp;
         private SerializedProperty obstacleLayerMaskProp;
 
+        private SerializedProperty spawnDuringTimesProp;
+        private SerializedProperty shardSpawnFromProp;
+        private SerializedProperty shardSpawnToProp;
+
         private void OnEnable()
         {
             shardPrefabProp = serializedObject.FindProperty("shardPrefab");
+            
             minSpawnTimeProp = serializedObject.FindProperty("minSpawnTime");
             maxSpawnTimeProp = serializedObject.FindProperty("maxSpawnTime");
+            spawnDuringTimesProp = serializedObject.FindProperty("spawnDuringTimeRange");
+            shardSpawnFromProp = serializedObject.FindProperty("shardSpawnFrom");
+            shardSpawnToProp = serializedObject.FindProperty("shardSpawnTo");
+            
             spawnOnTransformProp = serializedObject.FindProperty("spawnOnTransform");
             spawnBoxBoundsProp = serializedObject.FindProperty("spawnBoxBounds");
             respawnTypeProp = serializedObject.FindProperty("respawnType");
@@ -33,9 +42,25 @@ namespace Keegan.ShardSpawn
 
         public override void OnInspectorGUI()
         {
+            serializedObject.Update();
             EditorGUILayout.PropertyField(shardPrefabProp);
+            EditorGUILayout.Space();
+            
+            EditorGUILayout.LabelField("Spawn timing",  EditorStyles.boldLabel);
             EditorGUILayout.PropertyField(minSpawnTimeProp);
             EditorGUILayout.PropertyField(maxSpawnTimeProp);
+            EditorGUILayout.PropertyField(spawnDuringTimesProp);
+            
+            if (spawnDuringTimesProp.boolValue)
+            {
+                EditorGUILayout.BeginHorizontal();
+                EditorGUILayout.PropertyField(shardSpawnFromProp); 
+                EditorGUILayout.PropertyField(shardSpawnToProp);
+                EditorGUILayout.EndHorizontal();
+            }
+
+            EditorGUILayout.Space();
+            EditorGUILayout.LabelField("Base Spawn Settings");
             EditorGUILayout.PropertyField(spawnOnTransformProp);
             EditorGUILayout.PropertyField(spawnBoxBoundsProp);
             EditorGUILayout.PropertyField(groundLayerMaskProp);
