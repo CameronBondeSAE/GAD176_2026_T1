0c5582516c96834e735ba42046b96307ff1e491b
MitenPatel-dhunt3er1 <1036081@student.sae.edu.au>
2026-08-06 18:40:01 +1000

    portal now counts properlyy 2.5.21

--- Assets/Prefabs/Items/PortalStuffs/Scripts/PortalScript.cs
+++ Assets/Prefabs/Items/PortalStuffs/Scripts/PortalScript.cs
@@ -47,9 +47,9 @@ public class PortalScript : MonoBehaviour, IPowered
     bool CalculateNeededEnergy()
     {
         // Pass 1: Count total energy
+        totalEnergy = 0;
         foreach (Shard_Model shard in shardList)
         {
-            totalEnergy = 0;
             totalEnergy += shard.CurrentEnergy;
         }
 
