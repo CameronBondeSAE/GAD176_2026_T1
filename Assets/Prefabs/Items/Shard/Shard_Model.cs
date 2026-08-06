836ca8d96f670c2daa631ae7dc370e34972b2e54
KeeganMiller <keeganmiller.dev@gmail.com>
2026-08-06 11:56:25 +1000

    Added energy values

--- Assets/Prefabs/Items/Shard/Shard_Model.cs
+++ Assets/Prefabs/Items/Shard/Shard_Model.cs
@@ -1,9 +1,25 @@
 using Frank;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Shard_Model : MonoBehaviour, IPickup
 {
 	// Variables for totals, to be read by IPowered things
+	[SerializeField, Tooltip("Reference to the total power the shard has")]
+	private float maxEnergy = 100f;
+	[SerializeField, Tooltip("Reference to the current energy")]
+	private float currentEnergy;
+
+	/// <summary>
+	/// Reference to the current energy of this shard
+	/// </summary>
+	public float CurrentEnergy => currentEnergy;
+	/// </summary>
+		
+	private void Start()
+	{
+		currentEnergy = maxEnergy;
+	}
 	
 	public bool UseEnergy(int amount)
 	{
